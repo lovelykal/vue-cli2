ac0ba997ac314d2ae2a2ba835d3a48158c101dab
lovelykal <lovelykal@163.com>
2021-07-06 00:17:23 +0800

    master迁移至main

---
+++ src/views/first-step.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>my first step</h1>
+    <button @click="send">点我试试</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'FirstStep',
+  data () {
+    return {
+      zxc: {
+        asd: 123,
+        qwe: 456
+      },
+      msg: 'Welcome to my Fiest_step'
+    }
+  },
+  methods: {
+    send () {
+      axios.patch('url', this.zxc)
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
